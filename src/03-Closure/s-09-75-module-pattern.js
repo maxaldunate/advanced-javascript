28d4d2b19e6417c0e5cd387ec750397d417d56d9
Max Aldunate <maxaldunate@gmail.com>
2017-05-01 16:45:02 +0200

    module pattern

---
+++ src/03-Closure/s-09-75-module-pattern.js
@@ -0,0 +1,10 @@
+var foo = (function () {
+    var o = { bar: "bar" };
+    return {
+        bar: function () {
+            console.log(o.bar);
+        }
+    };
+})();
+
+foo.bar();   //bar
---
+++ src/03-Closure/s-10-76-modify-module-pattern.js
@@ -0,0 +1,14 @@
+var foo = (function () {
+    var publicAPI = {
+        bar: function () {
+            publicAPI.baz();
+        },
+        baz: function () {
+            console.log("baz");
+        }
+    };
+    return publicAPI;
+})();
+
+foo.bar();   //baz
+foo.baz();   //baz
