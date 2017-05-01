da5cb217aa9413f55b78596815c2c3b45e58927d
Max Aldunate <maxaldunate@gmail.com>
2017-05-01 19:01:44 +0200

    clieck event for click fix it

--- src/03-Closure/s-04-68-clicks.js
+++ src/03-Closure/s-04-68-clicks.js
@@ -1,9 +1,14 @@
 function foo() {
-    var bar = "bar";
+    var bar = 0;
 
     $("#btn").click(function (evt) {
-        console.log(bar);
+        console.log(bar++);
     });
 }
 
 foo();
+
+// At html file
+//<button id="btn" type="button">Click Me!</button>
+//<script src="jquery.js"></script>
+//<script src="03-Closure\s-04-68-clicks.js"></script>
