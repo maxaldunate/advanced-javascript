6ab863d16f5320b10aee0ec3bb5d9c169e84dd82
Max Aldunate <maxaldunate@gmail.com>
2017-05-05 10:48:58 +0200

    this keyword with event handlers

--- src/04-ObjectOrienting/s-07-104-this.redux.js
+++ src/04-ObjectOrienting/s-07-104-this.redux.js
@@ -2,10 +2,11 @@ function Foo(who) {
     this.me = who;
 }
 
-Foo.prototype.speak = function nombre() {
+Foo.prototype.speak = function() {
     console.log("Hello, " + this.me + ".");
 };
 
 var a1 = new Foo("a1");
 
-$("#speak").click(a1.speak);
+a1.speak();                     // It's works
+$("#speak").click(a1.speak);    // Doesen't work
