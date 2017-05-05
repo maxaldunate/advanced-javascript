ddc807899f91e488f489a876ab74b56ddadf06e7
Max Aldunate <maxaldunate@gmail.com>
2017-05-05 11:28:17 +0200

    split decl & init

--- src/04-ObjectOrienting/s-08-114-delegated-objects-step-3.js
+++ src/04-ObjectOrienting/s-08-114-delegated-objects-step-3.js
@@ -16,3 +16,30 @@ Bar.speak = function () {
 var b1 = Object.create(Bar);
 b1.init("b1");
 b1.speak();
+
+
+//Max's way
+var Foo2 = {
+    init: function Foo(who) {
+        this.me = who;
+        return this;
+    },
+    identify: function () {
+        return "I am " + this.me;
+    }
+};
+
+var Bar2 = Object.create(Foo2);
+
+Bar2.speak = function () {
+    console.log("Hello, " + this.identify() + ".");
+};
+
+// Split declaration and initialization
+var b2 = Object.create(Bar2);
+b2.init("b2");
+b2.speak();
+
+// One step for declaration and initialization
+var b3 = Object.create(Bar2).init("b3");
+b3.speak();
